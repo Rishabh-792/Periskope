c597c31c9ba5588344f50f492de85bbc29d55509
Rishabh Maheshwari <rishphalod7@gmail.com>
2025-05-26 15:15:30 +0530

    Changed project directory structure

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Only if you're using the App Router
-  },
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
